361a7cc5d91c26b9ce1743001f2dee3cf248641a
testname <test@test.com>
2015-03-09 19:44:55 +0100

    code restructered and birds added

--- OrbFeatureExtractor.py
+++ OrbFeatureExtractor.py
@@ -4,50 +4,56 @@ Created on 07.03.2015
 @author: zissi
 '''
 import cv2
-import Image
 import numpy as np
 from collections import Counter
 
 class OrbFeatureExtractor(object):
     '''
-    classdocs
+    Needs a list of images of equal size.
     '''
 
     def __init__(self, images):
         self.images = images
-        self.image_descriptors = []
-     
-    def extract(self, show_keypoints=False):
-        descriptors = np.ndarray((0, 32))
-        for image in self.images:
-            image_grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            orb = cv2.ORB_create()
-            keypoints = orb.detect(image_grey)
-            keypoints, descriptor = orb.compute(image_grey, keypoints)
-            self.image_descriptors.append(descriptor)
-            descriptors = np.vstack([descriptors, descriptor])
-            if show_keypoints:
-                image_new = np.ndarray(image_grey.shape)
-                image_new = cv2.drawKeypoints(image_grey, keypoints, image_new)
-                Image.fromarray(image_new).show()
-        return descriptors.astype(np.float32)
-
-    
-    def make_bag_of_visual_words(self):
-        bow_trainer = cv2.BOWKMeansTrainer(500)
-        bow_trainer.add(self.extract())
-        return bow_trainer.cluster()
         
-    def get_features(self):
+    def extract_features(self):
+        descriptors = self._calculate_descriptors()
+        vocabulary = self._make_bag_of_visual_words(descriptors)
+        features = self._calculate_features(descriptors, vocabulary)
+        return features
+     
+    def _calculate_descriptors(self):
+        '''extracts keypoints and descriptors from images.'''
+        image_descriptors = []
+        orb = cv2.ORB_create()
+        for image in self.images:
+            descriptor = self._get_descriptor(image, orb)
+            image_descriptors.append(descriptor)
+        return image_descriptors
+    
+    def _get_descriptor(self, image, orb):
+        image_grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # orb needs greyscale
+        keypoints = orb.detect(image_grey)
+        _, descriptor = orb.compute(image_grey, keypoints)
+        return descriptor
+       
+    def _make_bag_of_visual_words(self, descriptors):
+        '''creates codebook with 500 clusters'''
+        bow_trainer = cv2.BOWKMeansTrainer(500)
+        descriptors = [descriptor.astype(np.float32) for descriptor in descriptors]  # BOW needs 32bit floats for vocabulary creation
+        descriptors_array = np.vstack(descriptors)
+        bow_trainer.add(descriptors_array)
+        return bow_trainer.cluster()  # returns vocabulary as array of floats
+        
+    def _calculate_features(self, descriptors, vocabulary):
+        '''translates descriptors of each image into feature vector using the vocabulary'''
         features = []
-        vocabulary = self.make_bag_of_visual_words()
-        vocabulary = vocabulary.astype("uint8")
+        vocabulary = vocabulary.astype("uint8")  # BFMatcher needs ints
         matcher = cv2.BFMatcher()
-        for descriptor in self.image_descriptors:
-            matches = matcher.match(descriptor, vocabulary)
+        for descriptor in descriptors:
+            matches = matcher.match(descriptor, vocabulary)  # np.array of match objects between image descriptors and vocabulary
             feature = np.zeros(len(vocabulary), dtype=int)
-            hits = Counter([x.trainIdx for x in matches])
-            for idx in hits:
-                feature[idx] = hits[idx]
+            visual_word_counts = Counter([x.trainIdx for x in matches])
+            for visual_word_idx in visual_word_counts:
+                feature[visual_word_idx] = visual_word_counts[visual_word_idx]  # adds counts to feature list, to get zeros for not present visual words
             features.append(feature)
         return features
--- run_pynguin.py
+++ run_pynguin.py
@@ -14,6 +14,10 @@ if __name__ == '__main__':
     data_cleaner.images_folder = '/media/Daten/Downloads/penguin'
     clean_penguins = data_cleaner.collect_and_resize()
 
-    orb = OrbFeatureExtractor(clean_penguins)
-    # orb.make_bag_of_visual_words()
-    print orb.get_features()
+    orb = OrbFeatureExtractor(clean_penguins[:5])
+    penguin_features = orb.extract_features()
+    
+    orb.images = clean_birds[:5]
+    bird_features = orb.extract_features()
+    print bird_features
+    
